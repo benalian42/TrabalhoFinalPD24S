00c85656fe9da45e055b51dcea6adb642d56df36
benalian42 <117958226+benalian42@users.noreply.github.com>
2023-11-28 16:58:47 -0300

    Add files via upload

--- TrabalhoAvaliativo2/Form1.cs
+++ TrabalhoAvaliativo2/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,10 +13,31 @@ namespace TrabalhoAvaliativo2
 {
     public partial class Form1 : Form
     {
+        
+        private void tornarFullscreen()
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void telaAdministracao(  object sender, KeyEventArgs e )
+        {
+            if (e.Control && e.KeyCode == Keys.F5)
+            {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                
+            }
+            
+        }
+
         public Form1()
         {
             InitializeComponent();
             //teste
+            
+            tornarFullscreen();
+            this.KeyDown += telaAdministracao;
         }
 
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
